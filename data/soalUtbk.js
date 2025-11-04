2b7c48dcacdec253b54a26a12501f77f6f4ade7b
nouvalarrrgh <bagobago955@gmail.com>
2025-11-04 21:28:51 +0700

    Create soalUtbk.js

---
+++ data/soalUtbk.js
@@ -0,0 +1,5 @@
+export const soalUtbk = [
+  { id: 1, kategori: "PU", soal: "Jika semua A adalah B, dan semua B adalah C, maka ...", pilihan: ["Semua A adalah C","Semua C adalah A","Sebagian C bukan A","Tidak ada hubungan"], jawaban: 0 },
+  { id: 2, kategori: "PPU", soal: "Makna kata 'inovatif' ...", pilihan: ["Rajin","Mampu mencipta","Meniru","Cepat"], jawaban: 1 },
+  { id: 3, kategori: "PK", soal: "Jika 2x+3=9 maka x=?", pilihan: ["2","3","4","6"], jawaban: 0 }
+];
