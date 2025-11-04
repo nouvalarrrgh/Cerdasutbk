38456aa93fce4da5ecb6c8a0402aa6cc4d8f5e7d
nouvalarrrgh <bagobago955@gmail.com>
2025-11-04 23:10:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // penting untuk Netlify
-  images: { unoptimized: true },
+  output: 'export',
 };
 
 module.exports = nextConfig;
